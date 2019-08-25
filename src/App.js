a79a60de13c88d50002936b7b9b953113d57adc0
Garak <warrewp@protonmail.com>
2019-08-25 14:58:58 -0400

    Testing out branching

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Dolly! 123 Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
