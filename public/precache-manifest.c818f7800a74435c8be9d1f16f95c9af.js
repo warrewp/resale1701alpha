5f42f4277bd54ec92ae1978d3e969fdd1716ea3c
unknown <q.chen.3301@gmail.com>
2019-08-28 16:58:21 -0400

    ran npm install at root

---
+++ public/precache-manifest.c818f7800a74435c8be9d1f16f95c9af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94d3a34b7ec37ec95ad6919ab0300f96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c40045321fbc7203f28",
+    "url": "/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "bb882559393adc439f6f",
+    "url": "/static/js/2.55d4d49b.chunk.js"
+  },
+  {
+    "revision": "4c40045321fbc7203f28",
+    "url": "/static/js/main.2df40380.chunk.js"
+  },
+  {
+    "revision": "23fd513a5f786bbc0994",
+    "url": "/static/js/runtime~main.9d4b06a7.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
